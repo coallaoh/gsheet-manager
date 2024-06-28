f1e98ed0561961c87dd3f7a447904c91751adaf7
Seong Joon Oh <joon@seongs-mbp.speedport.ip>
2024-06-28 21:40:27 +0200

    Update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gsheet-manager",
-    version="0.1.0",
+    version="0.2.0",
     author="Seong Joon Oh",
     author_email="coallaoh@gmail.com",
     description="A Google Sheets manager for Python",
